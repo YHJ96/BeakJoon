b3df989fd046b83662141869fdc46193cbce6b8d
YHJ96 <yhj878812@naver.com>
2021-09-01 16:12:06 +0900

    YHJ96

---
+++ 10998/app.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+input = input[0];
+input = input.split(' ');
+input = input.map((item) => Number(item));
+
+solution(input[0], input[1]);
+
+function solution(a, b) {
+    const answer = a * b;
+    console.log(answer);
+}
