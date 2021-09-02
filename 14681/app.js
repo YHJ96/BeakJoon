34e543d4c6330dae8fad8964f8b4cd6f171d74b2
YHJ96 <yhj878812@naver.com>
2021-09-02 16:15:41 +0900

    YHJ96

---
+++ 14681/app.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+input = input.map((item) => parseInt(item));
+
+solution(input[0], input[1]);
+
+function solution(xAxis, yAxis) {
+    if(xAxis >= 0 && yAxis >= 0) console.log(1);
+    else if(xAxis < 0 && yAxis >= 0) console.log(2);
+    else if(xAxis < 0 && yAxis < 0) console.log(3);
+    else return console.log(4);
+}
