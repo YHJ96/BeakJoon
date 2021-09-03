85239a922386a90e2450fdd9b3462446f96fbe4f
YHJ96 <yhj878812@naver.com>
2021-09-03 13:34:12 +0900

    YHJ96

--- 2884/app.js
+++ 2884/app.js
@@ -8,11 +8,11 @@ input = input.map((item) => Number(item));
 solution(input[0], input[1]);
 
 function solution(hours, minutes) {
-    if(minutes <= 45) {
-        const minus = minutes - 45;
-        minutes = 60 + minus;
-        if(hours === 0) hours = 23;
-        else hours = hours - 1;
-    } else minutes = minutes - 45;
-    console.log(hours, minutes);
+    minutes = minutes - 45;
+    if(minutes < 0) {
+        minutes = minutes + 60;
+        hours = hours - 1;
+        if(hours === -1) hours = 23;
+    }
+    console.log(hours +' '+ minutes);
 }
