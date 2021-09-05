5c9b4b4c86b378067595d0e7d1aaab20af6e05ee
YHJ96 <yhj878812@naver.com>
2021-09-06 00:52:45 +0900

    YHJ96

---
+++ 3052/app.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
+input = input.map((item) => Number(item));
+
+solution(input);
+
+function solution(n) {
+    const filterNum = [];
+    n.forEach((item) => {
+        const num = item % 42;
+        if(filterNum.indexOf(num) === -1) {
+            filterNum.push(num);
+        }
+    })
+    console.log(filterNum.length);
+}
