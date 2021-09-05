dccc571641be11061a7980ae0120d6102815f0e9
YHJ96 <yhj878812@naver.com>
2021-09-05 12:35:43 +0900

    YHJ96

---
+++ 10952/app.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+const inputArray = [];
+for(let i = 0; i < input.length; i++) {
+    const inputValue = input[i].split(' ').map((item) => Number(item));
+    inputArray.push({ A : inputValue[0], B : inputValue[1] });
+}
+
+solution(inputArray);
+
+function solution(numArr) {
+    let answer = '';
+    let i = 0;
+    while(i < numArr.length) {
+        if(numArr[i].A === 0 && numArr[i].B === 0) break;
+        answer += `${numArr[i].A + numArr[i].B}` + '\n';
+        i++;
+    }
+    console.log(answer);
+};
