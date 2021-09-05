379cf0558b4629d01bfcd70a5dd66907d66f62f8
YHJ96 <yhj878812@naver.com>
2021-09-06 00:52:00 +0900

    YHJ96

---
+++ 10818/app.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+const inputArray = input[1].split(' ').map((item) => Number(item));
+
+solution(inputArray);
+
+function solution(n) {
+    let min = n[0];
+    let max = n[0];
+    for(let i = 0; i < n.length; i++) {
+        if(min > n[i]) min = n[i];
+        if(max < n[i]) max = n[i];
+    }
+    console.log(min, max);
+}
