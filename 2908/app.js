de00bc5742f7b771ef90cff8d350f5f2d327628a
YHJ96 <yhj878812@naver.com>
2021-09-09 10:19:54 +0900

    YHJ96

---
+++ 2908/app.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
+input = input[0].split(' ');
+
+solution(input[0], input[1]);
+
+function solution(num1, num2) {
+    let newNum1 = [...num1].reverse();
+    let newNum2 = [...num2].reverse();
+    newNum1 = +newNum1.join('');
+    newNum2 = +newNum2.join('');
+    const answer = Math.max(newNum1, newNum2);
+    console.log(answer);
+}
