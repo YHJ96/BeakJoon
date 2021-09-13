edb34b003ea44bc422be3e3d23acf670c4c1d841
YHJ96 <yhj878812@naver.com>
2021-09-13 14:09:49 +0900

    YHJ96

---
+++ 4153/app.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
+const inputArray = [];
+for(let i = 0; i < input.length; i++) {
+    const inputValue = input[i].split(' ').map((item) => Number(item));
+    inputArray.push(inputValue);
+}
+inputArray.pop();
+
+solution(inputArray);
+
+function solution(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        const max = Math.max(...arr[i]);
+        let sum = 0;
+        for(let j = 0; j < arr[i].length; j++) {
+            sum += arr[i][j] * arr[i][j];
+        }
+        if(sum - (max * max) === max * max) {
+            console.log('right');
+            sum = 0;
+        } else {
+            console.log('wrong');
+            sum = 0;
+        }
+    }
+}
