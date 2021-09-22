efcc90b3e849bf537206bf74d0f67831c1b89f00
YHJ96 <yhj878812@naver.com>
2021-09-22 13:18:20 +0900

    YHJ96

--- 2501/app.js
+++ 2501/app.js
@@ -6,12 +6,17 @@ input = input[0].split(' ').map((item) => +item);
 console.log(solution(input[0], input[1]));
 
 function solution(num, n) {
+    // 모든 약수를 answer에 넣기 위해 빈 배열 선언
     const answer = [];
+    // 1부터 num까지 모든 약수를 구하기 위해 반복문 사용
     for(let i = 1; i <= num; i++) {
+        // 나머지가 0이면 약수
         if(num % i === 0) {
             answer.push(i);
         }
     }
+    // n번쨰를 출력해야 하는데 n - 1이랑 answer의 길이가 같으면 0 예외처리
     if(answer.length <= n - 1) return 0;
+    // n번째 숫자 출력
     return answer[n - 1];
 }
