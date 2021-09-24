5c2aae24b59ba4f4a45ae9022f9a642483fcf0af
YHJ96 <yhj878812@naver.com>
2021-09-25 08:21:31 +0900

    YHJ96

--- 1952/app.js
+++ 1952/app.js
@@ -7,9 +7,12 @@ const findNum = input[3].split(' ').map((item) => +item);
 console.log(solution(numArr, findNum));
 
 function solution(arr, num) {
+    // 길이가 num이랑 같은 배열을 선언
     const result = Array.from({length : num.length}, () => 0);
+    // 인자를 set함수에 넣기
     const newArr = new Set(arr);
     for(let i = 0; i < num.length; i++) {
+        // 인자가 있으면 그 해당 자리수에 1을 더하기
         if(newArr.has(num[i])) result[i] += 1;
     }
     let answer = '';
