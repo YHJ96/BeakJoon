1ca98f9299116bdf6d92e271e3bf4409bc60b4d4
YHJ96 <yhj878812@naver.com>
2021-09-28 00:34:05 +0900

    YHJ96

--- 4948/app.js
+++ 4948/app.js
@@ -12,16 +12,21 @@ console.log(solution(inputArray));
 function solution(arr) {
     let answer = '';
     let result = [];
+    // 123456 * 2가 가장 큰 범위 이므로 그 범위안에 속하는 소수를 구하기 위해 배열 초기화
     for (let i = 2; i <= 246912; i++) {
         result[i] = i;
     }
+
+    // 범위 안에 있는 모든 소수 구하기 에라토스테네스의 체 사용
     for (let i = 2; i <= 246912; i++) {
         if (result[i] === 0) continue;
         for (let j = i + i; j <= 246912; j += i) {
             result[j] = 0;
         }
     }
+    // 0으로 만든 소수가 아닌 숫자들은 제거
     const filter = result.filter((item) => item !== 0);
+    // result에 있는 소수 범위에서 입력값으로 받아온 소수범위만 출력
     for (let i = 0; i < arr.length; i++) {
         let count = 0;
         for (let j = 0; j < filter.length; j++) {
@@ -33,3 +38,5 @@ function solution(arr) {
     }
     return answer.trim();
 }
+
+// 1. 에라토스테네스의 체 이용
