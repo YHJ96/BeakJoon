71e0484ce05a1ed4dc7017a3c71262bc81897676
YHJ96 <yhj878812@naver.com>
2021-09-29 06:58:48 +0900

    YHJ96

--- 3053/app.js
+++ 3053/app.js
@@ -6,6 +6,7 @@ input = +input[0];
 console.log(solution(input));
 
 function solution(n) {
+    // 택시의 기하학은 원을 직사각형으로 나타냄 직사각형의 넓이를 구하면 된다.
     let pi = Math.PI * n ** 2;
     let taxi = n ** 2 * 2;
     let answer = pi + '\n' + taxi;
