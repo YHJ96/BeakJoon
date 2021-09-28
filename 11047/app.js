e37c3a366a9c41b49a69addf53ab45a700cafec4
YHJ96 <yhj878812@naver.com>
2021-09-29 06:52:16 +0900

    YHJ96

--- 11047/app.js
+++ 11047/app.js
@@ -11,12 +11,15 @@ for(let i = 1; i < input.length; i++) {
 console.log(solution(price, coin));
 
 function solution(price, coin) {
+    // 최적의 조건은 price를 coin이 제일 큰 동전으로 나눠진 나머지를 다음 큰동전과 비교하는 것
     let answer = 0;
     coin.sort((a,b) => b-a);
     for(let x of coin) {
         if(price === 0) break;
         if(price >= x) {
+            // 나눈 값을 정수형으로 받는다.
             answer += parseInt(price / x);
+            // 값에 x로 나눈 나머지를 넣는다.
             price %= x;
         }
     }
