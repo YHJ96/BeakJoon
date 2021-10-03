7223cc23ff798471488f9d8a5404350eda054e40
YHJ96 <yhj878812@naver.com>
2021-10-03 15:31:18 +0900

    YHJ96

--- 1764/app.js
+++ 1764/app.js
@@ -13,11 +13,14 @@ for(let i = 1; i < input.length; i++) {
 console.log(solution(arr1, arr2));
 
 function solution(arr1, arr2) {
+    // filter를 이용해서 중복값 추출은 시간제한
+    // Obj를 이용해서 시간 줄이기
     const mySet = new Set(arr1);
     let answer = '';
     let count = 0;
     const result = [];
     for(let x of arr2) {
+        // mySet에 그 값이 있으면 count증가 시키고 result에 push
         if(mySet.has(x)) {
             count += 1;
             result.push(x);
