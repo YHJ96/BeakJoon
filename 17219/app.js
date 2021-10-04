91a1577e526ec42c828793747643e8ded1af5cd1
YHJ96 <yhj878812@naver.com>
2021-10-04 15:28:21 +0900

    YHJ96

--- 17219/app.js
+++ 17219/app.js
@@ -13,12 +13,15 @@ console.log(solution(file, search));
 
 function solution(file, search) {
     let answer = '';
+    // 키와 값으로 나누기 위해서 오브젝트 생성
     const myMap = new Map();
     for(let x of file) {
+        // 배열을 공백으로 나눈뒤 키를 사이트 주소 값을 비밀번호로 제작
         const item = x.split(' ');
         myMap.set(item[0], item[1]);
     }
     for(let x of search) {
+        // 객체에 있는 값을 가져오기
         answer += myMap.get(x) + '\n';
     }
     return answer.trim();
