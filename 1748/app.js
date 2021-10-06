fe4b7fd60ffc1d9b7329ff3b0d37871b46adb135
YHJ96 <yhj878812@naver.com>
2021-10-06 14:15:02 +0900

    YHJ96

--- 1748/app.js
+++ 1748/app.js
@@ -6,6 +6,7 @@ console.log(solution(+input));
 
 function solution(n) {
     let answer = 0;
+    // 1 ~ 9 에서는 자리수가 한개씩 10 ~ 99 자리수가 두개씩 쭉쭉 늘어나기 떄문에 자리수의 합만큼만 더해준다.
     for (let i = 1; i <= n; i *= 10) {
         answer += n - i + 1;
     }
