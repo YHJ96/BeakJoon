cdf919ae920ab2a8139908a6cad1cd53daba4a42
YHJ96 <yhj878812@naver.com>
2021-10-06 14:15:19 +0900

    YHJ96

---
+++ 1193/app.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
+input = +input;
+
+console.log(solution(input));
+
+function solution(n) {
+    let count = 1;
+    while(true) {
+        n -= count;
+        if(n <= 0) {
+            n += count;
+            break;
+        }
+        count += 1;
+    }
+    return (count % 2 === 1) ? `${count - (n - 1)}/${1 + (n - 1)}` : `${1 + (n - 1)}/${count - (n - 1)}`;
+}
