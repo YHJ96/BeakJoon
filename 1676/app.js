2f51e2cb692ebb44b0cff9c54ce468d0635678d5
YHJ96 <yhj878812@naver.com>
2021-10-10 15:23:34 +0900

    YHJ96

--- 1676/app.js
+++ 1676/app.js
@@ -6,6 +6,7 @@ input = +input[0];
 console.log(solution(input));
 
 function solution(n) {
+    // 결과값의 0을 알려면 2와 5의 배수를 알면 된다. 최소값이므로 5의 배수를 알면 되고 5를 곱하면서 진행하면서 5의 개수를 출력한다.
     let answer = 0;
     for(let i = 5; i <= n; i *= 5) {
         console.log(i);
