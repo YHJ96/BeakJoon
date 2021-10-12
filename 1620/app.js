078da1d60444458f35660ea008de50724029399f
YHJ96 <yhj878812@naver.com>
2021-10-12 12:43:24 +0900

    YHJ96

--- 1620/app.js
+++ 1620/app.js
@@ -13,14 +13,18 @@ console.log(solution(note, choice));
 function solution(note, choice) {
     let result = [];
     let answer = '';
+    // Map함수로 Obj 만들기
     const myMap = new Map();
+    // index + 1 의 키와 note의 인덱스 값을 지니고 note의 값의 키와 index + 1 값을 가지는 객체 생성
     for(let i = 0; i < note.length; i++) {
         myMap.set(`${i + 1}`, note[i]);
         myMap.set(note[i], i + 1);
     }
+    // choice에 해당되는 키의 값을 result에 넣기
     for(let x of choice) {
         result.push(myMap.get(x));
     }
+    // 정답출력 정제
     for(let x of result) {
         answer += x + '\n';
     }
