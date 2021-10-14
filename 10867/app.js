923cb1017ba16d5c7a8c9a6be01334b369dedad3
YHJ96 <yhj878812@naver.com>
2021-10-14 17:47:14 +0900

    YHJ96

--- 10867/app.js
+++ 10867/app.js
@@ -7,11 +7,15 @@ input = input[0].split(' ').map((item) => +item);
 console.log(solution(input));
 
 function solution(n) {
+    // set함수로 중복제거
     const mySet = new Set(n);
     let result = [];
+    // set의 인자를 result에 넣기
     for(let x of mySet) {
         result.push(x);
     }
+    // 오름차순 정렬
     result.sort((a,b) => a-b);
+    // 공백 넣어서 출력
     return result.join(' ');
 }
