50e625fee4e93df31750886fae2ecb7856bfff44
YHJ96 <yhj878812@naver.com>
2021-10-20 14:22:04 +0900

    YHJ96

--- 15829/app.js
+++ 15829/app.js
@@ -8,13 +8,17 @@ function solution(str) {
     let answer = 0;
     let count = 0n;
     let result = [];
+    // Hash 암호화 하기 위해 인덱스 지정
     const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
     for(let x of str) {
+        // index를 찾아서 result에 반환
         result.push(alphabet.indexOf(x) + 1);
     }
     for(let x of result) {
+        // 제곱근
         answer = BigInt(answer) + BigInt(x) * BigInt(31n ** count);
         count += 1n;
     }
+    // n이 뒤에 생기므로 문자열로 반환
     return (answer % 1234567891n).toString();
 }
