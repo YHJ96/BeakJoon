560c31f6c6f8b4322763c88cd81b612b7bda8ed6
YHJ96 <yhj878812@naver.com>
2021-10-21 20:06:31 +0900

    YHJ96

--- 1874/app.js
+++ 1874/app.js
@@ -18,14 +18,17 @@ function solution(num) {
     }
     for(let j = 0; j < num.length; j++) {
         let count = 1;
+        // count의 크기가 num의 크기와 작거나 같고 stack의 마지막 인자가 들어온 인자랑 같지 않을때 연산실행
         while(count <= num.length && stack[stack.length - 1] != num[j]) {
-            stack.push(arr.shift());
+            stack.push(arr.shift());       
             answer += '+' + '\n';
             count += 1; 
         }
+        // stack의 마지막 인자와 num의 인자와 같을때 연산실행
         if(stack[stack.length - 1] === num[j]) {
             stack.pop();
             answer += '-' + '\n';
+            // 둘의 조건이 맞지 않을 경우 NO return
         } else return 'NO';
     }
     return answer.trim();
