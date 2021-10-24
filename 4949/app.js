8814e7ae120fb885d270e9142d55151c3fb78245
YHJ96 <yhj878812@naver.com>
2021-10-24 16:16:19 +0900

    해결완료

--- 4949/app.js
+++ 4949/app.js
@@ -1,41 +1,42 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().trim().split('\n');
-input.pop();
+const inputArray = [];
+for(let x of input) {
+    inputArray.push(x.trim());
+}
+inputArray.pop();
 
-console.log(solution(input));
-// 실패 테스트 케이스 다시 수정 (괄호를 하나로 보고 풀어야할꺼 같음) 미해결
-function solution(arr) {
-    let answer = [];
-    for (let x of arr) {
-        if (x.indexOf('(') > x.indexOf(')') || x.indexOf('[') > x.indexOf(']')) {
-            answer.push('no');
-            continue;
-        } else if(x.indexOf('(') === -1 || x.indexOf(')') === -1 || x.indexOf('[') === -1 || x.indexOf(']') === -1) {
-            answer.push('yes');
-            continue;
-        } 
-        const item = x.trim();
-        const small = [];
-        const large = [];
-        for (let value of item) {
-            if(value === '(') small.push('X');
-            else if(value === ')') {
-                if(small.length === 0) {
-                    small.push('X');
-                    break;
-                } else small.pop();
-            }
-            else if(value === '[') large.push('X');
-            else if(value === ']') {
-                if(large.length === 0) {
-                    large.push('X');
-                    break;
-                } else large.pop();
-            }
+function checkItem(s) {
+    let answer = 'yes';
+    let stack = [];
+    const compare = ['[', ']', '(', ')'];
+    s = [...s].filter((item) => compare.includes(item));
+    if(s.length === 0) return answer;
+    if(s[0] !== '(' && s[0] !== '[') return 'no';
+    for(let i = 0; i < s.length; i++) {
+        const size = stack.length;
+        stack.push(s[i]);
+        if(s[i] === ')' && stack[size - 1] === '(' && i - 1 >= 0) {
+            stack.pop();
+            stack.pop();
         }
-        if(small.length !== 0 || large.length !== 0) answer.push('no');
-        else answer.push('yes');
+        if(s[i] === ']' && stack[size - 1] === '[' && i - 1 >= 0) {
+            stack.pop();
+            stack.pop();
+        }
     }
+    if(stack.length !== 0) answer = 'no';
     return answer;
 }
+
+function solution(arr) {
+    let answer = '';
+    for(let x of arr) {
+        const item = checkItem(x);
+        answer += item + '\n';
+    }
+    return answer.trim();
+}
+
+console.log(solution(inputArray));
