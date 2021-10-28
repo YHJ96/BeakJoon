ae0bbbf0772fdfc350a70051ba4642427200c8dd
YHJ96 <yhj878812@naver.com>
2021-10-28 17:08:08 +0900

    해결완료

--- 1032/app.js
+++ 1032/app.js
@@ -8,15 +8,19 @@ for(let i = 1; i < input.length; i++) {
 
 function solution(input) {
     let result = '';
+    // cmd창 문자 1개로 비교 시작
     for (let i = 0; i < input[0].length; i++) {
         let compare = input[0][i];
         let flag = true;
-        for (let j = 0; j < input.length; j++) {
+        // 0 기준으로 비교함으로 1부터 시작
+        for (let j = 1; j < input.length; j++) {
+            // 문자열이 다르면 false로 바꾼뒤 탈출
             if (compare !== input[j][i]) {
                 flag = false;
                 break;
             }
         }
+        // flag가 ture면 그대로 문자사용
         if (flag) result += compare;
         else result += "?";
     }
