639f87305649cae41864abc641fb11a112b12784
YHJ96 <yhj878812@naver.com>
2021-10-30 14:28:47 +0900

    해결완료

--- 1159/app.js
+++ 1159/app.js
@@ -7,17 +7,21 @@ for(let i = 1; i < input.length; i++) {
 }
 
 function solution(name) {
+    // 선수이름을 사전순으로 미리정렬
     name.sort();
     let answer = '';
     const myMap = new Map();
+    // 첫 스펠링이 있으면 Map에 넣어줌
     for(let x of name) {
         if(myMap.has(x[0])) {
             myMap.set(x[0], myMap.get(x[0]) + 1);
         } else myMap.set(x[0], 1);
     }
+    // 알파벳 갯수가 5개 이상인거만 추출
     myMap.forEach((item, index) => {
         if(myMap.get(index) >= 5) answer += index;
     })
+    // 예외처리
     if(answer.length === 0) answer = 'PREDAJA';
     return answer;
 }
