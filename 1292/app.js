47386d530eed3a9d0c159383b8c029f90f042d0a
YHJ96 <yhj878812@naver.com>
2021-10-31 19:06:16 +0900

    해결완료

--- 1292/app.js
+++ 1292/app.js
@@ -7,6 +7,7 @@ function solution(start, end) {
     let answer =  0;
     let numArr = [];
     let count = 0;
+    // 1 ~ n 까지의 반복 수열 만들기
     while(true) {
         for(let i = 0; i < count; i++) {
             numArr.push(count);
@@ -14,7 +15,9 @@ function solution(start, end) {
         if(count === Math.ceil(end / 2)) break;
         count += 1;
     }
+    // start 부터 end 까지의 합을 구하기
     for(let i = start; i <= end; i++) {
+        // 1부터 시작함으로 인덱스 0번이 첫번째 그러므로 - 1
         answer += numArr[i - 1];
     }
     return answer;
