bd45f4062a135165a5333f081ad20100a136eabb
YHJ96 <yhj878812@naver.com>
2021-11-06 16:59:47 +0900

    해결완료

--- 1302/app.js
+++ 1302/app.js
@@ -9,19 +9,24 @@ for(let i = 1; i < input.length; i++) {
 function solution(arr) {
     let answer = [];
     let max = 0;
+    // Map함수 사용
     const bH = new Map();
     for(let x of arr) {
+        // Map안에 키가없으면 1로 초기화 있으면 값 + 1
         if(bH.has(x)) bH.set(x, bH.get(x) + 1);
         else bH.set(x, 1);
     }
+    // 최대값 찾기
     bH.forEach((item, index) => {
         if(max <= item) {
             max = item;
         }
     });
+    // 중복체크
     bH.forEach((item, index) => {
         if(item === max) answer.push(index);
     });
+    // 오름차순 정렬
     answer.sort();
     return answer[0];
 }
