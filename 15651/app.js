a5668825a342dd472929af2113351ddb606e7a84
YHJ96 <yhj878812@naver.com>
2021-11-11 17:19:36 +0900

    해결완료

--- 15651/app.js
+++ 15651/app.js
@@ -5,12 +5,15 @@ input = input[0].split(' ').map((item) => +item);
 
 function solution(n, m) {
     let answer = '';
+    // 뽑는 개수를 지정하는 배열
     let tmp = Array.from({ length: m }, () => 0);
     function DFS(L) {
+        // 뽑는 개수와 같으면 answer 값 넣기
         if (L === m) {
             answer += tmp.slice().join(' ') + '\n';
         } else {
             for (let i = 1; i <= n; i++) {
+                // 1부터 시작
                 tmp[L] = i;
                 DFS(L + 1);
             }
