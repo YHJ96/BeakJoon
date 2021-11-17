df69501e619ecd8364f201b677adc9f487899123
YHJ96 <yhj878812@naver.com>
2021-11-17 17:12:33 +0900

    해결완료

--- 1743/app.js
+++ 1743/app.js
@@ -14,17 +14,24 @@ function solution(n, arr) {
     let row = n[1];
     let dy = [1,0,-1,0];
     let dx = [0,1,0,-1];
+    // 2차원 배열을 초기화
     const board = Array.from(Array(col), () => Array(row).fill('.'));
     for(let i = 0; i < arr.length; i++) {
+        // 좌표에 # 넣어주기
         const [y, x] = arr[i];
         board[y - 1][x - 1] = '#';
     }
+    // DFS
     function DFS(y, x) {
+        // 해당값을 .로 바꾸기
         board[y][x] = '.';
+        // count 갯수 1 증가
         count++;
+        // 4방 탐색 시작
         for(let k =0; k < dy.length; k++) {
             const ny = y + dy[k];
             const nx = x + dx[k];
+            // 4방 탐색 조건
             if(ny >= 0 && nx >= 0 && ny < col && nx < row && board[ny][nx] === '#') {
                 DFS(ny, nx);
             }
@@ -33,6 +40,7 @@ function solution(n, arr) {
 
     for(let i = 0; i < col; i++) {
         for(let j = 0; j < row; j++) {
+            // #을 찾으면 DFS시작
             if(board[i][j] === '#') {
                 DFS(i, j);
                 answer.push(count);
