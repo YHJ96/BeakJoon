74fc67bc656f19d8febd2f4025bc6892d235b805
YHJ96 <yhj878812@naver.com>
2021-11-19 12:49:48 +0900

    해결완료

--- 15652/app.js
+++ 15652/app.js
@@ -5,13 +5,17 @@ input = input[0].split(' ').map((item) => +item);
 
 function solution(n, m) {
     let answer = '';
+    // 뽑는 개수를 지정하는 배열
     let tmp = Array.from({length : m}, () => 0);
     function DFS(L, num) {
+        // 뽑는 개수와 같으면 answer 값 넣기
         if(L === m) {
             answer += tmp.join(' ') + '\n';
         } else {
             for(let i = num; i <= n; i++) {
+                // 1부터 시작
                 tmp[L] = i;
+                // 중복조합이므로 i그대로 가기
                 DFS(L+1, i);
             }
         }
