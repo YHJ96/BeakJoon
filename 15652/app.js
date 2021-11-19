ee0b8c0673f8a2992e92d29ae53d83c333ee1f23
YHJ96 <yhj878812@naver.com>
2021-11-19 12:48:46 +0900

    해결완료

---
+++ 15652/app.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
+input = input[0].split(' ').map((item) => +item);
+
+function solution(n, m) {
+    let answer = '';
+    let tmp = Array.from({length : m}, () => 0);
+    function DFS(L, num) {
+        if(L === m) {
+            answer += tmp.join(' ') + '\n';
+        } else {
+            for(let i = num; i <= n; i++) {
+                tmp[L] = i;
+                DFS(L+1, i);
+            }
+        }
+    }
+    DFS(0, 1);
+    return answer.trim();
+}
+
+console.log(solution(input[0], input[1]));
