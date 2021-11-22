0956e8eecea7ec4c8bfc2897459056ab7001e2ed
YHJ96 <yhj878812@naver.com>
2021-11-22 12:20:51 +0900

    해결완료

--- 9095/app.js
+++ 9095/app.js
@@ -6,11 +6,15 @@ input = input.map((item) => +item);
 
 function countNum(n) {
     let answer = 0;
+    // 1,2,3 으로만 숫자를 나타냄으로 기본 배열 선언
     const add = [1,2,3];
     function DFS(L, sum) {
+        // 해당 숫자보다 커지면 DFS 재귀 종료
         if(sum > n) return;
+        // 숫자가 같다면 answer을 1 증가
         if(sum === n) answer++;
         else {
+            // 반복문으로 백트래킹 시작
             for(let i = 0; i < add.length; i++) {
                 DFS(L+1, sum + add[i]);
             }
@@ -23,6 +27,7 @@ function countNum(n) {
 function solution(arr) {
     let answer = '';
     for(let x of arr) {
+        // 정답 출력
         answer += countNum(x) + '\n';
     }
     return answer.trim();
