2d57520b15f9563bcc84586cbb492357acabc882
YHJ96 <yhj878812@naver.com>
2021-11-23 14:56:40 +0900

    해결완료

--- 1431/app.js
+++ 1431/app.js
@@ -9,19 +9,25 @@ for (let i = 0; i < input.length; i++) {
 
 function solution(arr) {
     let answer = '';
+    // 정렬시작
     arr.sort((a, b) => {
+        // 문자열 길이가 작은순서대로 오름차순 정렬
         if(a.length < b.length) {
             return a.length - b.length;
         }
+        // 문자열 길이가 같다면 예외처리
         if(a.length === b.length) {
+            // 문자열 안에 있는 숫자들의 합이 작은순대로 오름차순 정렬
             const A = a.replace(/[a-z]/gi, "0").split('').map((item) => +item).reduce((acc, curr) => acc + curr);
             const B = b.replace(/[a-z]/gi, "0").split('').map((item) => +item).reduce((acc, curr) => acc + curr);
+            // 숫자도 같으면 사전순으로 정렬
             if(A === B) {
                 return a.localeCompare(b);
             }
             return A - B;
         }
     });
+    // 정답 정제
     for(let x of arr) {
         answer += x + '\n';
     }
