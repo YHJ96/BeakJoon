9ff5239640599e023bb0d793df24cb2172786e5f
YHJ96 <yhj878812@naver.com>
2021-11-25 18:49:53 +0900

    해결완료

--- 10825/app.js
+++ 10825/app.js
@@ -10,14 +10,18 @@ for(let i = 1; i < input.length; i++) {
 function solution(arr) {
     let answer = '';
     arr.sort((a,b) => {
+        // 국어점수 내림차순 정렬 시작
        if(a.korean < b.korean) return b.korean - a.korean;
        else if (a.korean > b.korean) return b.korean - a.korean;
+       // 국어점수가 같다면 영어점수 오름차순 정렬 시작
        else {
            if(a.english < b.english) return a.english - b.english;
            else if(a.english > b.english) return a.english - b.english;
+           // 국어점수와 영어점수가 같다면 수학점수 내림차순 정렬시작
            else {
                if(a.math < b.math) return b.math - a.math;
                else if(a.math > b.math) return b.math - a.math;
+               // 국어점수 영어점수 수학점수가 같다면 문자열 sort
                else {
                 if (a.name > b.name) return 1;
                 else if (a.name < b.name) return -1;
@@ -26,6 +30,7 @@ function solution(arr) {
            }
        }
     });
+    // 정답 정제
     for(let x of arr) {
         answer += x.name + '\n';
     }
