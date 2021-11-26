9d9e2351bb10d69c3a0ebcca6811d58c5177a5b7
YHJ96 <yhj878812@naver.com>
2021-11-26 11:16:16 +0900

    해결완료

--- 11004/app.js
+++ 11004/app.js
@@ -5,7 +5,9 @@ const inputArray = input[1].split(' ').map((item) => +item);
 const n = input[0].split(' ').map((item) => +item)[1];
 
 function solution(arr, n) {
+    // 오름차순 정렬
     arr.sort((a,b) => a-b);
+    // n 번쨰 숫자 출력 인덱스는 0 부터 시작
     const answer = arr[n - 1];
     return answer;
 }
