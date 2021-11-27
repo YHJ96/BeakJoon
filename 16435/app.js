c32fc4c2e63a582cce550923c6847a25d2e9d7e5
YHJ96 <yhj878812@naver.com>
2021-11-27 16:57:36 +0900

    해결완료

--- 16435/app.js
+++ 16435/app.js
@@ -6,8 +6,10 @@ const lang = input[0].split(' ').map((item) => +item)[1];
 
 function solution(lang, arr) {
     let answer = lang;
+    // 오름차순 정렬
     arr.sort((a,b) => a-b);
     for(let x of arr) {
+        // 길이를 비교해서 짧으면 +1를 해주면서 탐색
         if(answer >= x) answer += 1;
     }
     return answer;
