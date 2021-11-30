ab2e744d3fda4aaa3be80bfb176a1aa686dbc35b
YHJ96 <yhj878812@naver.com>
2021-11-30 17:39:20 +0900

    해결완료

--- 4796/app.js
+++ 4796/app.js
@@ -8,13 +8,20 @@ for(let i = 0 ; i < input.length; i++) {
     inputArray.push(inputValue);
 }
 
+// 휴가 계산 함수
 function checkDate(arr) {
     let answer = 0;
+    // P일중 L일을 사용가능하며 V가 휴가의 총일
     const [L,P,V] = arr;
+    // 휴가중 몇번 사용한지 계산
     const date = parseInt(V / P);
+    // 사용하고 남은 요일 계산
     const remainder = V % P;
+    // L일을 사용할수 있으므로 date에 곱한값 넣어주기
     answer = date * L;
+    // 나머지요일이 L일 보다 크면 L일 더하기
     if(remainder > L) answer += L;
+    // 아닐경우 나머지 요일 더하기
     else answer += remainder;
     return answer;
 }
@@ -22,6 +29,7 @@ function checkDate(arr) {
 function solution(arr) {
     let answer = '';
     let count = 0;
+    // 테스트 케이스 입력값 정제
     for(let x of arr) {
       const item = checkDate(x);
       count++;
