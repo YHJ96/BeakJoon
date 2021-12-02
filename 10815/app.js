1ec5537cbad5dba01c03f1ad8e5f797b864c4411
YHJ96 <yhj878812@naver.com>
2021-12-02 21:43:00 +0900

    해결완료

--- 10815/app.js
+++ 10815/app.js
@@ -6,12 +6,17 @@ const checkNum = input[3].trim().split(' ').map((item) => +item);
 
 function solution(num, checkNum) {
     let answer = '';
+    // num을 저장할 객체 Map함수로 생성
     let numHash = new Map();
     for(let x of num) {
+        // Map 객체에 인자가 없으면 추가
         if(!numHash.has(x)) numHash.set(x, 1);
     }
+    // Map안에 일치하는 숫자 확인
     for(let x of checkNum) {
+        // 기본값 설정
         let isCheck = 0;
+        // Map안에 인자가 있으면 1로 isCheck 재할당
         if(numHash.has(x)) isCheck = 1;
         answer += isCheck + ' ';
     }
