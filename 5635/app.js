bbed29de949b4eaedf8f3f7bac9222f0b7651ece
YHJ96 <yhj878812@naver.com>
2021-12-04 16:38:25 +0900

    해결완료

--- 5635/app.js
+++ 5635/app.js
@@ -11,15 +11,20 @@ for(let i = 0; i < input.length; i++) {
 
 function solution(arr) {
     let answer = '';
+    // 나이를 파악하기 위해 정렬 시작
     arr.sort((a,b) => {
+        // 년도 별로 정렬
         if(a[3] !== b[3]) return a[3] - b[3];
         else {
+            // 월 별로 정렬
             if(a[2] !== b[2]) return a[2] - b[2];
             else {
+                // 일 별로 정렬
                 return a[1] - b[1];
             }
         }
     });
+    // 나이가 최대인 사람과 최소인 사람 검색
     answer += arr[arr.length - 1][0];
     answer += '\n';
     answer += arr[0][0];
