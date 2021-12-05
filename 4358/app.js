dcb9fb08db1e709b82aa7dcf73dd40f4874824fa
YHJ96 <yhj878812@naver.com>
2021-12-05 15:01:15 +0900

    해결완료

--- 4358/app.js
+++ 4358/app.js
@@ -7,15 +7,20 @@ input.forEach((item, index) => {
 
 function solution(arr) {
     let answer = '';
+    // 사전순으로 정렬
     arr.sort();
     const treeHash = new Map();
     const total = arr.length;
     for(let x of arr) {
+        // 나무 이름과 개수를 저장
         if(!treeHash.has(x)) treeHash.set(x, 1);
         else treeHash.set(x, treeHash.get(x)+1);
     }
+    // 해쉬테이블을 순회
     treeHash.forEach((item, index) => {
+        // 백분율 구하기
         const percent = treeHash.get(index) / total * 100;
+        // 나무이름과 소수점 4번쨰 까지 정제
         answer += `${index} ${percent.toFixed(4)}` + '\n';
     });
     return answer.trim();
