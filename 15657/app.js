3187e73cff19f90f391786cf8bdbfd3d2ee70962
YHJ96 <yhj878812@naver.com>
2021-12-09 14:18:20 +0900

    해결완료

--- 15657/app.js
+++ 15657/app.js
@@ -6,7 +6,9 @@ const num = input[1].split(' ').map((item) => +item);
 
 function solution(n, num) {
     let answer = '';
+    // n만큼의 뽑는 개수를 지정
     let tmp = Array.from({length : n}, () => 0);
+    // 배열 오름차순 정리
     num.sort((a,b) => a-b);
     function DFS(L, s) {
         if(L === n) {
@@ -15,11 +17,13 @@ function solution(n, num) {
         } else {
             for(let i = s; i < num.length; i++) {
                 tmp[L] = num[i];
+                // 중복 조합이므로 그대로 가져가기
                 DFS(L+1, i);
             }
         }
     }
     DFS(0, 0);
+    // 정답 정제
     return answer.trim();
 }
 
