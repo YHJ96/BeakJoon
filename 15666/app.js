428d7ea0a5e1a9c3cfa87a03e81785c286245706
YHJ96 <yhj878812@naver.com>
2021-12-12 17:29:49 +0900

    해결완료

--- 15666/app.js
+++ 15666/app.js
@@ -5,14 +5,19 @@ const n = input[0].split(' ').map((item) => +item)[1];
 const num = input[1].split(' ').map((item) => +item);
 
 function solution(n, num) {
+    // 중복체크를 위해 Set함수 생성
     let answer = new Set();
+    // 오름차순 정렬
     num.sort((a,b) => a-b);
+    // n만큼을 뽑아서 만들기 위해 배열 선언
     const tmp = Array.from({ length : n }, () => 0);
     function DFS(L, s) {
         if(L === n) {
+            // 중복 제거를 위해 Set에 인자값 넣어주기
             const item = tmp.join(' ');
             answer.add(item);
         } else {
+            // 백트래킹 시작 조합 뽑기
             for(let i = s; i < num.length; i++) {
                 tmp[L] = num[i];
                 DFS(L+1, i);
