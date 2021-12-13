e345db0cae0ad9cd9d8adfb5c6983a0565772b45
YHJ96 <yhj878812@naver.com>
2021-12-13 14:17:31 +0900

    해결완료

--- 15664/app.js
+++ 15664/app.js
@@ -5,15 +5,19 @@ const n = input[0].split(' ').map((item) => +item)[1];
 const num = input[1].split(' ').map((item) => +item);
 
 function solution(n, num) {
+    // 중복제거를위한 set함수 생성
     let answer = new Set();
     num.sort((a,b) => a-b);
+    // n개를 뽑아서 조합할 빈 배열
     let tmp = Array.from({length : n}, () => 0);
+    // 인자를 사용했는지 체크 배열
     let ch = Array.from({ length : num.length }, () => 0);
     function DFS(L, s) {
         if(L === n) {
             const item = tmp.join(' ');
             answer.add(item);
         } else {
+            // 백트레킹 시작
             for(let i = s; i < num.length; i++) {
                 if(ch[i] === 0) {
                     tmp[L] = num[i];
