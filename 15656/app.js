054ae4e142ea44984f7e85cf5548732be2c4981b
YHJ96 <yhj878812@naver.com>
2021-12-14 12:23:27 +0900

    해결완료

--- 15656/app.js
+++ 15656/app.js
@@ -6,13 +6,17 @@ const num = input[1].split(' ').map((item) => +item);
 
 function solution(n, num) {
     let answer = [];
+    // n 개를 뽑아서 나열할 배열 선언
     let tmp = Array.from({length : n}, () => 0);
+    // 오름차순 정렬
     num.sort((a,b) => a-b);
     function DFS(L) {
+        // n개를 뽑았을 경우
         if(L === n) {
             const item = tmp.join(' ');
             answer.push(item);
         } else {
+            // 백트레킹 시작
             for(let i = 0; i < num.length; i++) {
                 tmp[L] = num[i];
                 DFS(L+1);
@@ -20,6 +24,7 @@ function solution(n, num) {
         }
     }
     DFS(0);
+    // 정답 정제
     return answer.join('\n');
 }
 
