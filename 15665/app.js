e0dd8b9e92ed44f93911176330c74c5035276fb6
YHJ96 <yhj878812@naver.com>
2021-12-15 13:22:37 +0900

    해결완료

--- 15665/app.js
+++ 15665/app.js
@@ -5,14 +5,18 @@ const n = input[0].split(' ').map((item) => +item)[1];
 const num = input[1].split(' ').map((item) => +item);
 
 function solution(n, num) {
+    // 중복제거를 위해 Set함수 호출
     let answer = new Set();
+    // 오름차순 정렬
     num.sort((a,b) => a-b);
+    // n개를 뽑는 배열
     let tmp = Array.from({length : n}, () => 0);
     function DFS(L) {
         if(L === n) {
             const item = tmp.join(' ');
             answer.add(item);
         } else {
+            // 백트레킹 시작
             for(let i = 0; i < num.length; i++) {
                 tmp[L] = num[i];
                 DFS(L+1);
