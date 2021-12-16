164f905821eb8c8d24aadce9bd52515c77324f34
YHJ96 <yhj878812@naver.com>
2021-12-16 11:48:40 +0900

    해결완료

--- 5568/app.js
+++ 5568/app.js
@@ -5,8 +5,11 @@ input.shift();
 
 function solution(arr) {
     const n = arr.shift();
+    // 중복 체크를 위한 Set함수 설정
     const answer = new Set();
+    // n개를 뽑는 경우의 수 배열
     let tmp = Array.from({length : n}, () => 0);
+    // 방문했는지 확인하는 배열
     let ck = Array.from({length : arr.length}, () => 0);
     function DFS(L) {
         if(L === n) {
@@ -14,6 +17,7 @@ function solution(arr) {
             answer.add(item);
         }
         else {
+            // 백트래킹 시작
             for(let i = 0; i < arr.length; i++) {
                 if(ck[i] === 0) {
                     ck[i] = 1;
