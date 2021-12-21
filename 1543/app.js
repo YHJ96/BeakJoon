d36554fc93b4308602d144e458f1101f1c5295c5
YHJ96 <yhj878812@naver.com>
2021-12-21 14:42:46 +0900

    해결완료

--- 1543/app.js
+++ 1543/app.js
@@ -5,7 +5,9 @@ const str = input[0].trim();
 const compare = input[1];
 
 function solution(str, compare) {
+    // 문서의 compare에 해당된 문자별로 나누고 0을 대입
     const item = str.split(compare).join('0');
+    // 0만 추출
     const answer = [...item].filter((item) => item === '0');
     return answer.length;
 }
