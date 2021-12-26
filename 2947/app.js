faa74495247b0c151cd286f7c58393116e8d2a69
YHJ96 <yhj878812@naver.com>
2021-12-26 12:05:18 +0900

    해결완료

--- 2947/app.js
+++ 2947/app.js
@@ -4,13 +4,18 @@ let input = fs.readFileSync(filePath).toString().trim().split('\n')[0].split(' '
 
 function solution(arr) {
     let answer = '';
+    // 버블정렬 실행
     for(let i = 0; i < arr.length; i++) {
         for(let j = 0; j < arr.length; j++) {
+            // j가 배열의 끝에 도착하면 정지
             if(j === arr.length) break;
+            // 앞에 있는 값이 뒤에 있는 값보다 크면 실행
             if(arr[j] > arr[j+1]) {
+                // 배열의 위치 바꾸기
                 const [acc, curr] = [arr[j], arr[j+1]];
                 arr[j] = curr;
                 arr[j+1] = acc;
+                // 위치를 바꿀경우 정답추출
                 answer += arr.join(' ') + '\n';
             }
         }
