e0abd0fd85bf8ff6df18cddf992dbfd101aa3888
YHJ96 <yhj878812@naver.com>
2021-12-28 15:09:16 +0900

    해결완료

--- 20291/app.js
+++ 20291/app.js
@@ -11,18 +11,24 @@ for(let x of input) {
 function solution(arr) {
     let answer = '';
     const result = [];
+    // 확장자를 확인할 해쉬테이블 생성
     const textHash = new Map();
     arr.forEach((item) => {
+        // 배열의 인자를 .을 기준으로 나눠서 확장자명만 추출
         const text = item.split('.')[1];
+        // Map함수에 있으면 1을 더하고 없으면 새로 지정
         if(!textHash.has(text)) textHash.set(text, 1);
         else textHash.set(text, textHash.get(text) + 1);
     });
     textHash.forEach((value, key) => {
+        // 확장자명의 이름과 개수를 저장
         result.push([key, value]);
     });
     result.sort((a,b) => {
+        // 사전순 정렬
         return a[0].localeCompare(b[0]);
     });
+    // 정답 정제
     for(let item of result) {
         const [name, count] = item;
         answer += `${name} ${count}` + '\n';
