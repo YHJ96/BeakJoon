e1cf853cf7247eda6500c3bb5f4282e5e56d4a6b
YHJ96 <yhj878812@naver.com>
2021-12-31 12:55:51 +0900

    해결완료

--- 7785/app.js
+++ 7785/app.js
@@ -9,14 +9,21 @@ for(let x of input) {
 }
 
 function solution(arr) {
+    // Map함수 생성
     const check = new Map();
     for(let x of arr) {
+        // 분할할당으로 사원의 이름과 상태를 출력
         const [name, state] = x.split(' ');
+        // state값이 enter이면 사원을 Map에 저장
         if(state === 'enter') check.set(name, state);
+        // state값이 enter가 아니면 삭제
         else check.delete(name);
     }
+    // Map함수의 키를 배열로 바꿈
     const answer = [...check.keys()];
+    // 사전순 내림차순 정렬
     answer.sort().reverse();
+    // 정답 정제
     return answer.join('\n');
 }
 
