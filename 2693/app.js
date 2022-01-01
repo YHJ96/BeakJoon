658c544a547c2c5bc34ba3d1c37bf70ab0e49c4a
YHJ96 <yhj878812@naver.com>
2022-01-01 10:03:44 +0900

    해결완료

--- 2693/app.js
+++ 2693/app.js
@@ -8,16 +8,21 @@ for(let i = 0; i < input.length; i++) {
     inputArray.push(item);
 }
 
+// 3번째 숫자 찾기
 function findNum(arr) {
+    // 내림차순 정렬
     arr.sort((a,b) => b-a);
+    // 분할할당으로 3번째 값 추출
     const [one, two, three] = arr;
     return three;
 }
 
 function solution(arr) {
     let answer = '';
+    // 배열의 요소를 findNum에 전달
     for(let x of arr) {
         const item = findNum(x);
+        // 정답 정제
         answer += item + '\n';
     }
     return answer.trim();
