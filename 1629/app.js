12411b3fd481211dd24a884a2651d10d150e94e8
YHJ96 <yhj878812@naver.com>
2022-02-11 09:55:26 +0900

    해결완료

--- 1629/app.js
+++ 1629/app.js
@@ -1,14 +1,19 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().trim().split('\n');
-input = input[0].split(' ').map((item) => +item);
-console.log(solution(input));
+input = input[0].split(' ').map((item) => BigInt(item));
+const [a, b, c] = input;
 
-// 미해결
-function solution(num) {
-    const [a, b, c] = num;
-    const answer = BigInt(a) ** BigInt(b) % BigInt(c);
-    return answer.toString();
+// a를 b번 곱한뒤 c의 나머지
+function solution(a, b, c) {
+    // a^1 = a
+    if(b === 1n) return a % c;
+    // a^n = a^(n/2) * a^(n/2) 랑 같으므로 재귀호출
+    let answer = solution(a, BigInt(parseInt(b / 2n)), c);
+    answer = (answer * answer) % c;
+    // 만약 홀수이면 a^(n/2) * a^(n/2) 의 값에 * a
+    if(b % 2n === 1n) answer = (answer * a) % c;
+    return answer;
 }
 
-// 1. 분할정복
+console.log(Number(solution(a, b, c)));
