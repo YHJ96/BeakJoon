c4a8bc2d64ca3b878f5ca76253a0f10d6ef7f294
YHJ96 <yhj878812@naver.com>
2022-02-13 12:06:28 +0900

    해결완료

--- 11328/app.js
+++ 11328/app.js
@@ -5,7 +5,9 @@ let input =fs.readFileSync(filePath).toString().trim().split('\n').map((item) =>
 function solution(input) {
     const [s1, s2] = input;
     let answer = 0;
+    // 문자열 2개를 각각 알파벳으로 나누기위해서 2차원 배열 초기화
     const compare = Array.from({length : 2}, () => Array.from({length : 26}, () => 0));
+    // 2개의 문자열을 돌면서 알파벳 순서대로 있으면 +1씩 증가
     for(let i = 0; i < s1.length; i++) {
         const idx = s1.codePointAt(i) - 97;
         compare[0][idx]++;
@@ -14,6 +16,7 @@ function solution(input) {
         const idx = s2.codePointAt(i) - 97;
         compare[1][idx]++;
     }
+    // 단어의 절대값의 차를 구하기
     for(let i = 0; i < 26; i++) answer += Math.abs(compare[0][i] - compare[1][i]);
     return answer;
 } 
