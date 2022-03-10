6695850e05cd900062cce428631f285f0009fe27
YHJ96 <yhj878812@naver.com>
2022-03-10 15:57:41 +0900

    해결완료

--- 2776/app.js
+++ 2776/app.js
@@ -1,24 +1,48 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().trim().split('\n');
-const noteOne = input[2].split(' ').map((item) => item.trim());
-const noteTwo = input[4].split(' ').map((item) => item.trim()); 
-
-// 해결실패 테스트케이스 문제인지 아직 모르겠음 
-function solution(noteOne, noteTwo) {
-    const answer = Array.from({ length : noteOne.length }, () => 0);
-    const noteHash = new Set();
-    
-    for(let x of noteOne) {
-        noteHash.add(x)
+const inputArray = [];
+input.shift();
+let cnt = [];
+for(let i = 0; i < input.length; i++) {
+    if(i % 2 === 1) {
+        const inputValue = input[i].split(" ").map((item) => +item);
+        cnt.push(inputValue);
     }
+    if(cnt.length === 2) {
+        inputArray.push(cnt);
+        cnt = [];
+    }
+}
+// 테스트 케이스가 N개임
 
-    noteTwo.forEach((item, index) => {
-        if(noteHash.has(item)) answer[index] = 1;
-        else answer[index] = 0;
+// 두개의 배열중 첫번 째의 요소인 배열에 포함되어 있는지 확인하는 함수
+function serachNum(arr) {
+    let answer = [];
+    // 구조분해할당으로 arr 배열을 나누기
+    const [one, two] = arr;
+    // set 함수로 중복제거
+    const ck = new Set();
+    // one의 모든 요소를 ck에 등록
+    one.forEach((item) => ck.add(item));
+    // ck에 요소가 없으면 0을 push하고 있으면 1을 push
+    two.forEach((item) => {
+        if(ck.has(item)) answer.push(1);
+        else answer.push(0);
     });
-
-    return answer.join('\n');
+    // 요소 정제
+    return answer.join("\n");
 }
 
-console.log(solution(noteOne, noteTwo));
+function solution(arr) {
+    const answer = [];
+    // arr의 요소만큼 요소에 대하여 실행
+    for(let x of arr) {
+        const item = serachNum(x);
+        answer.push(item);
+    }
+    // 정답 정제
+    return answer.join("\n");
+}
+
+console.log(solution(inputArray));
