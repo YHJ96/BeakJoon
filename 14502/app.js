a1f5f030d8f2904a20790827835590cc42484a70
YHJ96 <yhj878812@naver.com>
2022-07-11 10:57:41 +0900

    해결완료

--- 14502/app.js
+++ 14502/app.js
@@ -7,52 +7,66 @@ for(let i = 0; i < input.length; i++) {
     inputArray.push(inputValue);
 }
 
-// 백트래킹 말고 완탐으로 해보기
 function solution(arr) {
     let answer = 0;
+    const virus = [];
+    const section = [];
     const [[n,m], ...board] = arr;
-    function dfs(l) {
-        if (l === 3) {
-            let count = 0;
-            const newBoard = [];
-            for(let item of board) newBoard.push(item.slice());
-            const dy = [1,0,-1,0];
-            const dx = [0,1,0,-1];
-            const queue = [];
-            for(let i = 0; i < n; i++) {
-                for(let j = 0; j < m; j++) {
-                    if (newBoard[i][j] === 2) queue.push([i, j]);
-                }
-            }
-            while(queue.length) {
-                const [y, x] = queue.shift();   
-                newBoard[y][x] = 2;
-                for(let i = 0; i < 4; i++) {
-                    const ny = y + dy[i];
-                    const nx = x + dx[i];
-                    if(ny < 0 || nx < 0 || ny >= n || nx >= m || newBoard[ny][nx] !== 0) continue;
-                    queue.push([ny, nx]);
-                }
-            }
-            for(let i = 0; i < n; i++) {
-                for(let j = 0; j < m; j++) {
-                    if (newBoard[i][j] === 0) count += 1;
-                }
-            }
-            answer = Math.max(count, answer);
-        } else {
-            for(let i = 0; i < n; i++) {
-                for(let j = 0; j < m; j++) {
-                    if (board[i][j] === 0) {
-                        board[i][j] = 1;
-                        dfs(l+1);
-                        board[i][j] = 0;
+    // 완전 탐색으로 바이러스와 구역 좌표 모두 구하기
+    for(let i = 0; i < n; i++) {
+        for(let j = 0; j < m; j++) {
+            if (board[i][j] === 0) section.push([i, j]);
+            if (board[i][j] === 2) virus.push([i, j]);
+        }
+    }
+    // 구역에서 3가지를 뽑는 경우의 수 완전탐색 시작
+    for(let i = 0; i < section.length - 2; i++) {
+        for(let j = i + 1; j < section.length - 1; j++) {
+            for(let k = j + 1; k < section.length; k++) {
+                let count = 0;
+                // 복사를 위해서 map과 queue 초기화
+                const map = [];
+                const queue = [];
+                // 4방 탐색 좌표
+                const dy = [1,0,-1,0];
+                const dx = [0,1,0,-1];
+                // 깊은 복사 시작
+                for(let position of board) map.push(position.slice());
+                for(let position of virus) queue.push(position.slice());
+                // 좌표 구조 분해 할당
+                const [y1, x1] = section[i];
+                const [y2, x2] = section[j];
+                const [y3, x3] = section[k];
+                // 벽 세우기
+                map[y1][x1] = 1;
+                map[y2][x2] = 1;
+                map[y3][x3] = 1;
+                // 너비 우선 탐색 시작
+                while(queue.length) {
+                    const [y, x] = queue.shift();
+                    // 방문 처리
+                    map[y][x] = 2;
+                    // 4방 탐색 시작
+                    for(let i = 0; i < 4; i++) {
+                        const ny = y + dy[i];
+                        const nx = x + dx[i];
+                        // 예외처리
+                        if (ny < 0 || nx < 0 || ny >= n || nx >= m || map[ny][nx] !== 0) continue;
+                        // 좌표 push
+                        queue.push([ny, nx]);
                     }
                 }
+                // 안전구역 검색
+                for(let i = 0; i < n; i++) {
+                    for(let j = 0; j < m; j++) {
+                        if (map[i][j] === 0) count += 1;
+                    }
+                }
+                // 최대값 정제
+                answer = Math.max(answer, count);
             }
         }
     }
-    dfs(0);
     return answer;
 }
 
